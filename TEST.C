17350d6b513a92aa07a2e93bdb7583c56591132c
UmaSankar29 <93313608+UmaSankar29@users.noreply.github.com>
2021-10-29 20:48:17 -0700

    Add files via upload

---
+++ TEST.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int val1, val2, val3;
+   scanf("%d",&val1);
+   printf("%d\n", val1);
+   getch();
+   scanf("%d",&val2);
+   printf("%d\n", val2);
+   getch();
+   scanf("%d",&val3);
+   printf("%d", val3);
+   getch();
+}
